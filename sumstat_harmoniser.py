53e0f9d8f1396a018b45732b76ac80e2d3a88f8a
Edward Mountjoy <em21@mib111400i.windows.ebi.ac.uk>
2017-11-20 16:58:00 +0000

    removed pseudocode from main script

--- sumstat_harmoniser.py
+++ sumstat_harmoniser.py
@@ -7,56 +7,6 @@
 #
 # Harmonise GWAS summary statistics against a reference VCF
 
-"""
-# Pseudo code:
-
-Load summary stat record (ssrec).
-
-Get record from reference VCF (vcfrec) using ssrec chrom:pos and tabix.
-  If no record -> discard (code: ??) and break
-  Assert that only 1 record was returned (should be the case for gnomad vcfs at least)
-  Discard alt alleles if allele freq is below af_vcf_min threshold
-  If no alt alleles remaining -> discard (code: ??) and break
-
-As vcfrec could be multialleleic, compare ssrec alleles to vcfrec alleles.
-Search for ssrec allele match with each vcfrec ref-alt allele pair and their reverse complements.
-Remove alt allele and corresponding INFO entries if not a match.
-  If no matches -> discard ssrec as ambiguous (code: ??) and break
-  If >1 match -> discard ssrec as ambiguous (code: ??) and break
-  If 1 match -> proceed
-
-Check whether ssrec alleles and vcfrec alleles are palindromic
-  If palindromic
-    If infer_palin==True and infer_strand==True:
-      If MAF of either ssrec or vcfrec alleles is > maf_palin_infer_threshold -> discard (error code: ??)
-      If ssrec and vcfrec EAFs are not concordant (one is >0.5 and other <0.5)
-        Flip beta (code: ??) and break
-      Else:
-        Don't flip beta (code: ??) and break
-    Else: -> discard (code: ??) and break
-
-Check whether on the opposite strand: ssrec alleles == reverse_complement(vcfrec alleles)
-  If opposite strand
-    If infer_strand==True
-      If ssrec effect_al == reverse_complement(vcfrec effect_al):
-        Don't flip beta (code: ??) and break
-      Else:
-        Flip beta (code: ??) and break
-    Else:
-      Assuming forward strand therefore alleles are ambiguous -> discard (code: ??) and break
-
-Check that alleles are on the same strand (they should be by now).
-  If same stand:
-    If ssrec effect_al == vcfrec effect_al:
-      Don't flip beta (code: ??) and break
-    Else:
-      Flip beta (code: ??) and break
-
-Else error (shouldn't ever get here):
-  Raise error
-
-"""
-
 import os
 import sys
 import gzip
@@ -65,7 +15,7 @@ from subprocess import Popen, PIPE
 from collections import OrderedDict
 
 def main():
-    """ Implements main logic
+    """ Implements main logic.
     """
 
     # Get args
